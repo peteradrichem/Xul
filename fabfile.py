6fe7e058552cef566ebf318a81492a5527f7fcec
Peter Adrichem <Peter.Adrichem@gmail.com>
2014-08-26 22:30:18 +0200

    fabfile.py t.b.v. local en remote hg tip via `fab tip' toegevoegd

---
+++ fabfile.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+# vim: set encoding=utf-8 :
+
+"""Send Remote Commands Via SSH"""
+
+
+# Fabric imports
+from fabric.api import env, task, cd
+from fabric.operations import local
+
+# Brik imports
+from brik.runs import cmd_run
+
+# Deze fabfile is voor hg.omroep.nl
+from brik.config import apps_dict
+env.hosts = apps_dict['hg']
+env.remote_repo_path = "ab/Xml"
+
+
+@task
+def tip():
+    """ : local and remote Xml script repository tip """
+    local("hg tip")
+    with cd(env.remote_repo_path):
+        cmd_run("hg tip")
+
+@task
+def host():
+    """ : Toon remote `fab' host(s) """
+    for remote_h in env.hosts:
+        print(remote_h)
