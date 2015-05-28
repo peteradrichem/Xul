4ba0859fa3a2197af8c6a1115f25f7ce7589ba00
Peter Adrichem <Peter.Adrichem@gmail.com>
2015-05-28 21:21:27 +0200

    Only execute when running as the __main__ module (validate 2.0.0)

--- validate.py
+++ validate.py
@@ -13,7 +13,7 @@ from tab import setup_logger_console
 from tab.xml import build_xml_tree, build_xml_schema, build_dtd
 
 
-__version_info__ = ('1', '9', '1')
+__version_info__ = ('2', '0', '0')
 __version__ = '.'.join(__version_info__)
 
 def parse_cl():
@@ -37,49 +37,50 @@ def parse_cl():
     return parser.parse_args()
 
 
-# Logging to the console (TAB modules)
-setup_logger_console()
+if __name__ == '__main__':
+    # Logging to the console (TAB modules)
+    setup_logger_console()
 
-# Command-line
-(options, xml_files) = parse_cl()
+    # Command-line
+    (options, xml_files) = parse_cl()
 
-# XSD or DTD Validator?
-if options.xsd_file:
-    validator = build_xml_schema(options.xsd_file)
-    val_file = options.xsd_file
-    val_type = "XSD"
-elif options.dtd_file:
-    validator = build_dtd(options.dtd_file)
-    val_file = options.dtd_file
-    val_type = "DTD"
-else:
-    validator = None
-    val_file = None
-# Check validator
-if not val_file:
-    stderr.write('No XSD or DTD file specified\n')
-    exit(105)
-elif not validator:
-    # Warnings via build_xml_schema of build_dtd
-    stderr.write('Invalid %s file specified\n' % val_type)
-    exit(105)
+    # XSD or DTD Validator?
+    if options.xsd_file:
+        validator = build_xml_schema(options.xsd_file)
+        val_file = options.xsd_file
+        val_type = "XSD"
+    elif options.dtd_file:
+        validator = build_dtd(options.dtd_file)
+        val_file = options.dtd_file
+        val_type = "DTD"
+    else:
+        validator = None
+        val_file = None
+    # Check validator
+    if not val_file:
+        stderr.write('No XSD or DTD file specified\n')
+        exit(105)
+    elif not validator:
+        # Warnings via build_xml_schema of build_dtd
+        stderr.write('Invalid %s file specified\n' % val_type)
+        exit(105)
 
-# Validate XML files
-if not xml_files:
-    stderr.write("Valid %s file '%s'\n" % (val_type, val_file))
-    stderr.write("No XML file(s) to operate on\n")
-    exit(0)
-for xml_f in xml_files:
-    # Opm: build_xml_tree rapporteert XML fouten in xml_f
-    xml_tree = build_xml_tree(xml_f)
-    if xml_tree:
-        # Probeer de ElementTree te valideren
-        if validator.validate(xml_tree):
-            print "XML file '%s' validates against '%s'" % (xml_f, val_file)
-        else:
-            stderr.write(
-                "XML file '%s' does not validate against '%s':\n" %
-                (xml_f, val_file))
-            for e in validator.error_log:
+    # Validate XML files
+    if not xml_files:
+        stderr.write("Valid %s file '%s'\n" % (val_type, val_file))
+        stderr.write("No XML file(s) to operate on\n")
+        exit(0)
+    for xml_f in xml_files:
+        # Opm: build_xml_tree rapporteert XML fouten in xml_f
+        xml_tree = build_xml_tree(xml_f)
+        if xml_tree:
+            # Probeer de ElementTree te valideren
+            if validator.validate(xml_tree):
+                print "XML file '%s' validates against '%s'" % (xml_f, val_file)
+            else:
                 stderr.write(
-                    "\tline %i, column %i: %s\n" % (e.line, e.column, e.message))
+                    "XML file '%s' does not validate against '%s':\n" %
+                    (xml_f, val_file))
+                for e in validator.error_log:
+                    stderr.write(
+                        "\tline %i, column %i: %s\n" % (e.line, e.column, e.message))
