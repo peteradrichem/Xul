e20a83192a3293c93e5b75f8102b5cfc6967b4af
Peter Adrichem <Peter.Adrichem@gmail.com>
2015-05-28 21:05:37 +0200

    Comments, docstrings and parse_cl()

--- validate.py
+++ validate.py
@@ -1,10 +1,10 @@
 #!/usr/local/bin/python -t
 # coding=utf-8
 
-""" Validate XML files with a XSD or DTD file """
+"""Validate XML files with a XSD or DTD file."""
 
 
-# Standard Python modules
+# Standard Python
 from optparse import OptionParser
 from sys import stderr
 #
@@ -13,25 +13,18 @@ from tab import setup_logger_console
 from tab.xml import build_xml_tree, build_xml_schema, build_dtd
 
 
-# Versie
 __version_info__ = ('1', '9', '1')
 __version__ = '.'.join(__version_info__)
 
-description = "Validate XML files with a XSD or DTD file"
-epilog = "Documentation: http://docu.npoict.nl/applicatiebeheer/documentatie/xml_scripts"
-
 def parse_cl():
-    """ Lees de command-line options uit.
-        Geef opties en bestanden lijst terug
-        - options.xsd_file: XSD file
-        - options.dtd_file: DTD file
-        - args: files list
-    """
-    usage = """\t%prog -x xsd_file xml_file_1 ... xml_file_n
-\t%prog -d dtd_file xml_file_1 ... xml_file_n"""
+    """Parse the command-line for options and XML files."""
     parser = OptionParser(
-        usage=usage, description=description,
-        epilog=epilog, version="%prog " + __version__)
+        usage="""\t%prog -x xsd_file xml_file_1 ... xml_file_n
+\t%prog -d dtd_file xml_file_1 ... xml_file_n""",
+        description=__doc__,
+        epilog="Documentation: " +
+        "http://docu.npoict.nl/applicatiebeheer/documentatie/xml_scripts",
+        version="%prog " + __version__)
     parser.add_option(
         "-x", "--xsd",
         action="store", type="string", dest="xsd_file",
@@ -41,17 +34,16 @@ def parse_cl():
         action="store", type="string", dest="dtd_file",
         help="DTD file to validate XML file(s)")
 
-    # Parse script's command line
     return parser.parse_args()
 
 
-# Logging op het console
+# Logging to the console (TAB modules)
 setup_logger_console()
 
-# Command-line parsen: XSD/DTD file & XML files
+# Command-line
 (options, xml_files) = parse_cl()
 
-# XSD of DTD?
+# XSD or DTD Validator?
 if options.xsd_file:
     validator = build_xml_schema(options.xsd_file)
     val_file = options.xsd_file
@@ -63,24 +55,20 @@ elif options.dtd_file:
 else:
     validator = None
     val_file = None
-#
-# Validator OK?
+# Check validator
 if not val_file:
-    # Geen file meegegeven
     stderr.write('No XSD or DTD file specified\n')
     exit(105)
 elif not validator:
     # Warnings via build_xml_schema of build_dtd
     stderr.write('Invalid %s file specified\n' % val_type)
     exit(105)
-#
-# XML bestand meegegeven?
+
+# Validate XML files
 if not xml_files:
     stderr.write("Valid %s file '%s'\n" % (val_type, val_file))
     stderr.write("No XML file(s) to operate on\n")
     exit(0)
-
-# Loop de XML files af
 for xml_f in xml_files:
     # Opm: build_xml_tree rapporteert XML fouten in xml_f
     xml_tree = build_xml_tree(xml_f)
