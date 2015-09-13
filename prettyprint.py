f77f1066b538f88ccd20c7451ae74a96db2c5559
Peter Adrichem <Peter.Adrichem@gmail.com>
2015-09-13 21:54:48 +0200

    Moved prettyprint() to new xul.ppxml
    --HG--
    branch : xul

--- prettyprint.py
+++ prettyprint.py
@@ -10,11 +10,12 @@ from sys import stdout, stderr, stdin
 #
 # pylint: disable=no-name-in-module
 # lxml ElementTree <http://lxml.de/>
-from lxml.etree import XMLParser, tostring
+from lxml.etree import XMLParser
 #
 # Xul modules
 from xul.log import setup_logger_console
 from xul.dom import build_etree
+from xul.ppxml import prettyprint
 
 
 __version_info__ = ('1', '1', '0')
@@ -33,40 +34,12 @@ def parse_cl():
     return cl_parser.parse_args()
 
 
-def pyg_pprint(etree):
-    """Pretty print XML ElementTree in (Pygments) color."""
-    xml_str = tostring(
-        etree, encoding='UTF-8',
-        xml_declaration=True, pretty_print=True)
-    print highlight(xml_str, lexer, formatter)
-
-def no_pyg_pprint(etree):
-    """Pretty print XML ElementTree."""
-    etree.write(
-        stdout, encoding='UTF-8',
-        xml_declaration=True, pretty_print=True)
-
-
 if __name__ == '__main__':
     # Logging to the console (TAB modules)
     setup_logger_console()
 
     # Command-line
     (options, xml_files) = parse_cl()
-
-    if options.color:
-        try:
-            from pygments.lexers import get_lexer_by_name
-            from pygments.formatters.terminal256 import Terminal256Formatter
-            from pygments import highlight
-        except ImportError:
-            prettyprint = no_pyg_pprint
-        else:
-            lexer = get_lexer_by_name('xml', encoding='utf-8')
-            formatter = Terminal256Formatter(encoding='utf-8', nobold=True)
-            prettyprint = pyg_pprint
-    else:
-        prettyprint = no_pyg_pprint
 
     # Initialise XML parser and remove blank text for 'pretty_print' formatting
     #   http://lxml.de/FAQ.html#parsing-and-serialisation
@@ -78,7 +51,7 @@ if __name__ == '__main__':
         xml_tree = build_etree(xml_f, parser=parser)
         if xml_tree:
             try:
-                prettyprint(xml_tree)
+                prettyprint(xml_tree, options.color)
                 # Voorkom "close failed in file object destructor:" meldingen
                 # bij meerdere XML bestanden en 'Broken pipe'
                 stdout.flush()
@@ -91,4 +64,4 @@ if __name__ == '__main__':
     if not xml_files:
         xml_tree = build_etree(stdin, parser=parser)
         if xml_tree:
-            prettyprint(xml_tree)
+            prettyprint(xml_tree, options.color)
---
+++ xul/ppxml.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+
+"""Pretty print XML."""
+
+
+# Standard Python
+from sys import stdout
+#
+# pylint: disable=no-name-in-module
+# lxml ElementTree <http://lxml.de/>
+from lxml.etree import tostring
+
+
+def no_color_pp(etree):
+    """Pretty print XML ElementTree without color."""
+    etree.write(
+        stdout, encoding='UTF-8',
+        xml_declaration=True, pretty_print=True)
+
+
+try:
+    from pygments.lexers import get_lexer_by_name
+    from pygments.formatters.terminal256 import Terminal256Formatter
+    from pygments import highlight
+except ImportError:
+    # pylint: disable=unused-argument
+    def prettyprint(etree, color=False):
+        """Redirect to no_color_pp."""
+        return no_color_pp(etree)
+else:
+    lexer = get_lexer_by_name('xml', encoding='utf-8')
+    formatter = Terminal256Formatter(encoding='utf-8', nobold=True)
+    def prettyprint(etree, color=True):
+        """Pretty print XML ElementTree in (Pygments) color."""
+        if color:
+            xml_str = tostring(
+                etree, encoding='UTF-8',
+                xml_declaration=True, pretty_print=True)
+            print highlight(xml_str, lexer, formatter)
+        else:
+            return no_color_pp(etree)
