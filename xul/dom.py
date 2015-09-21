413fc82ab07286d93536666f8ee937d01c3636e7
Peter Adrichem <Peter.Adrichem@gmail.com>
2015-09-21 19:19:34 +0200

    xml_source instead of file_obj/xml_file: an XML file or file-like object (stdin). Comments and docstrings (PEP 257)
    --HG--
    branch : xul

--- xul/dom.py
+++ xul/dom.py
@@ -1,16 +1,14 @@
 # coding=utf-8
 
-"""XML Document Object Model.
+"""XML Document Object Model / ElementTree.
 
-Document Object Model / ElementTree
-
-W3C Document Object Model
+W3C Document Object Model:
     http://www.w3.org/DOM/
 
-The ElementTree XML API
+The ElementTree XML API:
     https://docs.python.org/library/xml.etree.elementtree.html
 
-ElementTree Overview
+ElementTree Overview:
     http://effbot.org/zone/element-index.htm
 """
 
@@ -27,38 +25,46 @@ from lxml import etree
 logger = getLogger(__name__)
 
 
-def build_etree(file_obj, parser=None, lenient=True):
-    """Parse file object into an XML Document Object Model (ElementTree).
-       - file_obj: file object
-       - parser: (optional) lxml.etree.XMLParser object
-       - lenient: log XMLSyntaxError as warnings instead of errors
+def build_etree(xml_source, parser=None, lenient=True):
+    """Parse XML source into an XML Document Object Model (ElementTree).
 
-       Return XML Document Object Model on success.
-       Return None on error.
+    xml_source -- XML file or file-like object
+    parser -- (optional) XML parser (lxml.etree.XMLParser)
+    lenient -- log XMLSyntaxError as warnings instead of errors
 
-       Extensible Markup Language (XML):
-           http://www.w3.org/XML/
+    Return XML Document Object Model on success.
+    Return None on error.
 
-       Uses lxml.etree.parse and lxml.etree.XMLParser
-           http://lxml.de/parsing.html
-           http://effbot.org/elementtree/elementtree-xmlparser.htm
-           http://effbot.org/zone/element.htm#reading-and-writing-xml-files
+    Extensible Markup Language (XML):
+        http://www.w3.org/XML/
+
+    The lxml.etree.parse function:
+        http://lxml.de/parsing.html
+        http://effbot.org/zone/element.htm#reading-and-writing-xml-files
+    Parser options (lxml.etree.XMLParser class):
+        http://lxml.de/parsing.html#parser-options
+        http://effbot.org/elementtree/elementtree-xmlparser.htm
     """
-    # XML parser object preparation.
-    #   http://lxml.de/parsing.html#parser-options
+    # XML parser preparation.
     if not parser:
         parser = etree.XMLParser(ns_clean=True)
 
     try:
-        xml_dom = etree.parse(file_obj, parser)
-    # Catch XML exceptions.
+        xml_dom = etree.parse(xml_source, parser)
+    # Catch XML syntax errors.
     #   http://lxml.de/api.html#error-handling-on-exceptions
     except etree.XMLSyntaxError as inst:
         if lenient:
             xmllogger = logger.warning
         else:
             xmllogger = logger.error
-        xmllogger("%s is not an XML object:", file_obj)
+        if hasattr(xml_source, "name"):
+            name = xml_source.name
+            xml_type = "object"
+        else:
+            name = xml_source
+            xml_type = "file"
+        xmllogger("%s is not a valid XML %s:", name, xml_type)
         # http://lxml.de/parsing.html#error-log
         # [http://lxml.de/api/lxml.etree._LogEntry-class.html]
         for e in inst.error_log:
@@ -79,35 +85,37 @@ def build_etree(file_obj, parser=None, lenient=True):
 
 
 def build_xsl_transform(xslt_file):
-    """ Bouw een XSL transformer object op uit een XSLT bestand
-        - xslt_file: XSLT bestand
+    """Parse an XSLT file into an XSL Transformer.
 
-        Geeft lxml.etree.XSLT object terug.
-        Bij problemen:
-            - XSLT fouten worden gelogd als warnings
-            - als resultaat wordt None teruggegeven
+    xslt_file -- XSLT file (XML file)
 
-        Extensible Stylesheet Language Transformatie (XSLT)
-            http://www.w3.org/Style/XSL/
+    Lines with XSLT parse errors are logged as warnings.
+
+    Return XSL Transformer (lxml.etree.XSLT) on success.
+    Return None on error.
+
+    Extensible Stylesheet Language Transformatie (XSLT):
+        http://www.w3.org/Style/XSL/
+
+    The lxml.etree.XSLT class:
+        http://lxml.de/api/lxml.etree.XSLT-class.html
+        http://lxml.de/xpathxslt.html#xslt
+    I/O access control in XSLT:
+        http://lxml.de/resolvers.html#i-o-access-control-in-xslt
     """
-    # XSLT bestand dient valide XML te zijn
-    xslt_tree = build_etree(xslt_file, lenient=False)
-    if not xslt_tree:
+    xslt_etree = build_etree(xslt_file, lenient=False)
+    if not xslt_etree:
         return None
 
-    # Probeer een XSL Transformatie object op te bouwen (XSL transformer)
-    #   http://lxml.de/xpathxslt.html#xslt
-    # I/O access control in XSLT
-    #   http://lxml.de/resolvers.html
     try:
-        xsl_transform = etree.XSLT(xslt_tree)
-    # Vang XSLT fouten af
+        xsl_transform = etree.XSLT(xslt_etree)
+    # Catch XSLT parse errors.
     except etree.XSLTParseError as inst:
         logger.error("XML file '%s' is not a valid XSLT file", xslt_file)
         if not inst.error_log:
             logger.error("XSLTParseError: %s", inst)
         for e in inst.error_log:
-            # Bijv. e.level_name: "ERROR", e.domain_name: "XSLT", e.type_name: "ERR_OK"
+            # E.g. e.level_name: "ERROR", e.domain_name: "XSLT", e.type_name: "ERR_OK"
             if e.line == 0:
                 logger.error(e.message)
             else:
@@ -119,22 +127,23 @@ def build_xsl_transform(xslt_file):
 
 def etree_transformer(xml_dom, transformer, **params):
     """Transform an XML DOM (ElementTree) with an XSL Transformer.
-       - xml_dom: XML Document Object Model
-       - transformer: XSL Transformer (lxml.etree.XSLT)
-       - params: (optional) XSL parameters
-            http://lxml.de/xpathxslt.html#stylesheet-parameters
 
-       Return XML Document Object Model on success.
-       Return None on error.
+    xml_dom -- XML Document Object Model
+    transformer -- XSL Transformer (lxml.etree.XSLT)
+    params -- (optional) XSL style sheet parameters:
+        http://lxml.de/xpathxslt.html#stylesheet-parameters
 
-       XSL file lines with errors are logged as warnings
+    XSL file lines with apply errors are logged as warnings.
+
+    Return XML Document Object Model on success.
+    Return None on error.
     """
     try:
         if params:
             xml_result = transformer(xml_dom, **params)
         else:
             xml_result = transformer(xml_dom)
-    # Catch XSL Transformation errors
+    # Catch XSL Transformation errors.
     except etree.XSLTApplyError as inst:
         if not inst.error_log:
             logger.error("XSLTApplyError: %s", inst)
@@ -151,16 +160,17 @@ def etree_transformer(xml_dom, transformer, **params):
         return xml_result
 
 
-def xml_transformer(xml_file, transformer, parser=None):
+def xml_transformer(xml_source, transformer, parser=None):
     """Transform an XML file with an XSL Transformer.
-       - xml_file: XML file
-       - transformer: XSL Transformer (lxml.etree.XSLT)
-       - parser: (optional) lxml.etree.XMLParser object
 
-       Return XML Document Object Model on success.
-       Return None on error.
+    xml_source -- XML file or file-like object
+    transformer -- XSL Transformer (lxml.etree.XSLT)
+    parser -- (optional) XML parser (lxml.etree.XMLParser)
+
+    Return XML Document Object Model on success.
+    Return None on error.
     """
-    xml_dom = build_etree(xml_file, parser=parser, lenient=False)
+    xml_dom = build_etree(xml_source, parser=parser, lenient=False)
     if not xml_dom:
         return None
 
@@ -168,41 +178,43 @@ def xml_transformer(xml_file, transformer, parser=None):
     if xml_result:
         return xml_result
     else:
-        logger.error("XSL transformation on '%s' failed", xml_file)
+        if hasattr(xml_source, "name"):
+            name = xml_source.name
+        else:
+            name = xml_source
+        logger.error("XSL transformation on '%s' failed", name)
         return None
 
 
 def build_xml_schema(xsd_file):
-    """ Bouw een XMLSchema validator object op uit een XSD bestand
-        - xsd_file: XSD bestand
+    """Parse an XSD file into an XMLSchema Validator.
 
-        Geeft lxml.etree.XMLSchema object terug.
-        Bij problemen:
-            - XSD fouten worden gelogd als warnings
-            - als resultaat wordt None teruggegeven
+    xsd_file -- XSD file (XML file)
 
-        XML Schema Definition (XSD)
-            http://www.w3.org/XML/Schema
+    Lines with XSD parse errors are logged as warnings.
 
-        Gebruikt lxml.etree.XMLSchema
-            http://lxml.de/validation.html#xmlschema
-            http://lxml.de/api/lxml.etree.XMLSchema-class.html
+    Return XMLSchema Validator (lxml.etree.XMLSchema) on success.
+    Return None on error.
+
+    XML Schema Definition (XSD):
+        http://www.w3.org/XML/Schema
+
+    The lxml.etree.XMLSchema class:
+        http://lxml.de/validation.html#xmlschema
+        http://lxml.de/api/lxml.etree.XMLSchema-class.html
     """
-    # XSD bestand dient valide XML bestand te zijn
-    xsd_tree = build_etree(xsd_file, lenient=False)
-    if not xsd_tree:
+    xsd_etree = build_etree(xsd_file, lenient=False)
+    if not xsd_etree:
         return None
 
-    # Probeer een XMLSchema validator object op te bouwen
-    #   http://lxml.de/validation.html#xmlschema
     try:
-        validator = etree.XMLSchema(xsd_tree)
-    # Vang XSD fouten af
+        validator = etree.XMLSchema(xsd_etree)
+    # Catch XSD parse errors.
     except etree.XMLSchemaParseError as inst:
         if inst.error_log.last_error.line != 0:
             logger.error("XML file '%s' is not a valid XSD file", xsd_file)
         for e in inst.error_log:
-            # Bijv. e.level_name: "ERROR", e.domain_name: "SCHEMASP",
+            # E.g. e.level_name: "ERROR", e.domain_name: "SCHEMASP",
             # e.type_name: "SCHEMAP_NOT_SCHEMA"
             if e.line == 0:
                 # The XML document '%s' is not a schema document.
@@ -215,30 +227,30 @@ def build_xml_schema(xsd_file):
 
 
 def build_dtd(dtd_file):
-    """ Bouw een DTD validator object op uit een DTD bestand
-        - dtd_file: DTD bestand
+    """Parse an DTD file into an DTD Validator.
 
-        Geeft lxml.etree.DTD object terug.
-        Bij problemen:
-            - DTD fouten worden gelogd als warnings
-            - als resultaat wordt None teruggegeven
+    dtd_file -- DTD file
 
-        Document Type Definition (DTD)
-            http://en.wikipedia.org/wiki/Document_Type_Definition
+    Lines with DTD parse errors are logged as warnings.
 
-        Gebruikt lxml.etree.DTD
-            http://lxml.de/validation.html#id1
+    Return DTD Validator (lxml.etree.DTD) on success.
+    Return None on error.
+
+    Document Type Definition (DTD):
+        http://en.wikipedia.org/wiki/Document_Type_Definition
+
+    The lxml.etree.DTD class:
+        http://lxml.de/validation.html#id1
+        http://lxml.de/api/lxml.etree.DTD-class.html
     """
-    # Probeer de DTD file te parsen
-    #   http://lxml.de/validation.html#id1
     try:
         validator = etree.DTD(file=dtd_file)
-    # Vang DTD fouten af
-    #   "failed to load external entity" bij niet bestaand bestand
+    # Catch DTD parse errors
     except etree.DTDParseError as inst:
+        # "failed to load external entity" when file does not exist
         logger.error("'%s' is not a DTD file:", dtd_file)
         for e in inst.error_log:
-            # Bijv. e.level_name: "FATAL", e.domain_name: "PARSER",
+            # E.g. e.level_name: "FATAL", e.domain_name: "PARSER",
             # e.type_name: "ERR_EXT_SUBSET_NOT_FINISHED"
             if e.line == 0:
                 logger.error(e.message)
@@ -250,33 +262,30 @@ def build_dtd(dtd_file):
 
 
 def xml_validator(xml_file, validator):
-    """ Valideer een XML bestand tegen een XSD/DTD validator
-        - xml_file: XML bestand
-        - validator: XMLSchema (lxml.etree.XMLSchema) of DTD validator (lxml.etree.DTD)
+    """Validate an XML file against an XSD or DTD validator.
 
-        Validatie fouten worden gelogd als warnings
+    xml_file -- XML file
+    validator -- XMLSchema or DTD Validator
 
-        Geeft een tuple terug met het resultaat van de validatie (True/False)
-        en string met status tekst
+    Log XML validation errors as warnings.
+
+    Return a tuple with the validation result (True/False) and the status string.
     """
-    # Maak XML Document Object Model van het XML bestand
     xml_dom = build_etree(xml_file)
     if not xml_dom:
         return (False, "Not an XML file")
 
-    # Probeer de XML DOM (ElementTree) te valideren
     if validator.validate(xml_dom):
         logger.info("XML file '%s' validates", xml_file)
         return (True, "XML file validates")
     else:
-        # Error is onnodig luid bij ongeldige leverancier XML
         logger.warning("XML file '%s' does not validate:", xml_file)
         for e in validator.error_log:
-            # Bijv. DTD e.level_name: "ERROR", e.domain_name: "VALID",
+            # E.g. DTD e.level_name: "ERROR", e.domain_name: "VALID",
             # e.type_name: "DTD_UNKNOWN_ELEM"
-            # Bijv. XSD e.level_name: "ERROR", e.domain_name: "SCHEMASV",
+            # E.g. XSD e.level_name: "ERROR", e.domain_name: "SCHEMASV",
             # e.type_name: "SCHEMAV_CVC_ELT_1"
             logger.warning("line %i, column %i: %s", e.line, e.column, e.message)
-        # Geef alleen de eerste validatie fout terug
+        # Return the first line with a validation error (status string)
         e = validator.error_log[0]
         return (False, "line %i, column %i: %s" % (e.line, e.column, e.message))
