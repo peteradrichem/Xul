fd758db43b6bb31a788b308d775fab1e7772103f
Peter Adrichem <Peter.Adrichem@gmail.com>
2015-10-29 22:24:15 +0100

    validate script operates on XML sources: files, URLs and stdin. The XSD source can be a file or an URL. Ditto the DTD source

--- xul/cmd/validate.py
+++ xul/cmd/validate.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-"""Validate XML file(s) with an XSD or DTD file."""
+"""Validate XML source with XSD or DTD."""
 
 
 # Standard Python
@@ -14,29 +14,29 @@ from ..dom import build_etree, build_xml_schema, build_dtd
 
 
 def parse_cl():
-    """Parse the command-line for options and XML files."""
+    """Parse the command-line for options and XML sources."""
     parser = OptionParser(
-        usage="""\t%prog -x xsd_file xml_file_1 ... xml_file_n
-\t%prog -d dtd_file xml_file_1 ... xml_file_n""",
+        usage="""\t%prog -x xsd_source xml_source ...
+\t%prog -d dtd_source xml_source ...""",
         description=__doc__,
         version="%prog " + __version__)
     parser.add_option(
         "-x", "--xsd",
-        action="store", type="string", dest="xsd_file",
-        help="XSD file to validate XML file(s)")
+        action="store", type="string", dest="xsd_source",
+        help="XML Schema Definition (XSD) source")
     parser.add_option(
         "-d", "--dtd",
-        action="store", type="string", dest="dtd_file",
-        help="DTD file to validate XML file(s)")
+        action="store", type="string", dest="dtd_source",
+        help="Document Type Definition (DTD) source")
     return parser.parse_args()
 
 
-def validate_xml(xml_source, validator, val_file):
+def validate_xml(xml_source, validator, val_source):
     """Validate XML source against an XSD or DTD validator.
 
-    xml_source -- XML file or file-like object
-    validator -- XMLSchema or DTD Validator
-    val_file -- Validator file (XSD or DTD)
+    xml_source -- XML file, file-like object or URL
+    validator -- XSD or DTD Validator
+    val_source -- Validator source (XSD or DTD)
     """
     xml_dom = build_etree(xml_source)
     if xml_dom:
@@ -45,10 +45,10 @@ def validate_xml(xml_source, validator, val_file):
         else:
             name = xml_source
         if validator.validate(xml_dom):
-            print "'%s' validates against '%s'" % (name, val_file)
+            print "'%s' validates against '%s'" % (name, val_source)
         else:
             stderr.write(
-                "'%s' does not validate against '%s':\n" % (name, val_file))
+                "'%s' does not validate against '%s':\n" % (name, val_source))
             for e in validator.error_log:
                 stderr.write(
                     "\tline %i, column %i: %s\n" % (e.line, e.column, e.message))
@@ -60,35 +60,35 @@ def main():
     setup_logger_console()
 
     # Command-line
-    (options, xml_files) = parse_cl()
+    (options, xml_sources) = parse_cl()
 
     # XSD or DTD Validator?
-    if options.xsd_file:
-        validator = build_xml_schema(options.xsd_file)
-        val_file = options.xsd_file
+    if options.xsd_source:
+        validator = build_xml_schema(options.xsd_source)
+        val_source = options.xsd_source
         val_type = "XSD"
-    elif options.dtd_file:
-        validator = build_dtd(options.dtd_file)
-        val_file = options.dtd_file
+    elif options.dtd_source:
+        validator = build_dtd(options.dtd_source)
+        val_source = options.dtd_source
         val_type = "DTD"
     else:
         validator = None
-        val_file = None
+        val_source = None
     # Check validator
-    if not val_file:
-        stderr.write('No XSD or DTD file specified\n')
+    if not val_source:
+        stderr.write('No XSD or DTD source specified\n')
         exit(105)
     elif not validator:
-        stderr.write('Invalid %s file specified\n' % val_type)
+        stderr.write('Invalid %s source specified\n' % val_type)
         exit(105)
 
-    # Validate XML files and URLs
-    for xml_f in xml_files:
-        validate_xml(xml_f, validator, val_file)
+    # Validate XML sources
+    for xml_s in xml_sources:
+        validate_xml(xml_s, validator, val_source)
 
-    if not xml_files:
-        # Read from a pipe when no XML is specified
+    if not xml_sources:
+        # Read from a pipe when no XML source is specified
         if not stdin.isatty():
-            validate_xml(stdin, validator, val_file)
+            validate_xml(stdin, validator, val_source)
         else:
-            stderr.write("Error: no XML is given\n")
+            stderr.write("Error: no XML source specified\n")
