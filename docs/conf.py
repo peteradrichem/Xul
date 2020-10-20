7a26d8d21f66d23d51bc9d89f00b44a8e9945831
Peter Adrichem <Peter.Adrichem@gmail.com>
2020-10-20 20:59:55 +0200

    Cleaned up Sphinx imports; removed make.bat.

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,14 @@
 
 import sys
 import os
-import shlex
 from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-
 import xul
+
 
 # -- General configuration ------------------------------------------------
 
@@ -51,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Xul'
-copyright = u'%d Peter Adrichem' % datetime.now().year
+copyright = u'%d, Peter Adrichem' % datetime.now().year
 author = u'Peter Adrichem'
 
 # The version info for the project you're documenting, acts as replacement for
