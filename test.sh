a300985586ca3a9bc6e941840cabfbcac20fc821
Peter Adrichem <Peter.Adrichem@gmail.com>
2025-01-04 16:37:15 +0100

    Docker compose with test script for local testing.

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -u # crash on missing env
+set -e # stop on any error
+
+echo() {
+   builtin echo -e "$@"
+}
+
+
+echo "Lint checks (Ruff)"
+ruff check --diff
+
+echo "\nCheck import sort (isort)"
+isort --check --diff .
+
+echo "\nCheck formatting (Black)"
+black --check --diff .
+
+echo "\nCheck typing (mypy)"
+mypy .
+
+echo "\nChecks complete"
